3448b2532e3c42d4756d73b225c9c816ee2da3b1
Noel <noelcerulean@aol.com>
2021-11-09 15:06:09 -0500

    indentation fix

--- data/abilities.ts
+++ data/abilities.ts
@@ -1532,14 +1532,14 @@ export const Abilities: {[abilityid: string]: AbilityData} = {
 		onResidualOrder: 5,
 		onResidualSubOrder: 3,
         onResidual(pokemon) {
-            const ally = pokemon.side.pokemon[this.random(6) - 1];
-            // remove "&& ally !== pokemon" if you want the pokemon with healer to be able to heal itself
-            if (ally.hp && ally.status && ally !== pokemon && this.randomChance(30, 100)) {
-                this.debug('healer');
-                // this should make the activate text appear
-                if (ally.cureStatus()) this.add('-activate', ally, 'ability: Healer'); 
-            }
-        },
+			const ally = pokemon.side.pokemon[this.random(6) - 1];
+			// remove "&& ally !== pokemon" if you want the pokemon with healer to be able to heal itself
+			if (ally.hp && ally.status && ally !== pokemon && this.randomChance(30, 100)) {
+				this.debug('healer');
+				// this should make the activate text appear
+				if (ally.cureStatus()) this.add('-activate', ally, 'ability: Healer'); 
+			}
+		},
 		rating: 3,
 		num: 131,
 	},
