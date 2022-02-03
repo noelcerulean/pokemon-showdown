c166b4957b4dabac9971e4b39c322c5363cc8d26
Noel <noelcerulean@aol.com>
2022-02-03 18:30:49 -0500

    amaura and aurorus

--- data/mods/gen7/formats-data.ts
+++ data/mods/gen7/formats-data.ts
@@ -3988,7 +3988,7 @@ export const FormatsData: {[k: string]: ModdedSpeciesFormatsData} = {
 	aurorus: {
 		randomBattleMoves: ["ancientpower", "blizzard", "earthpower", "freezedry", "hypervoice", "stealthrock", "thunderwave"],
 		randomDoubleBattleMoves: ["ancientpower", "earthpower", "freezedry", "hypervoice", "icywind", "protect", "thunderwave"],
-		tier: "PU",
+		tier: "UU",
 		doublesTier: "(DUU)",
 	},
 	sylveon: {
