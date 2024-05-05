d95f7dcb0393ecf451da1b96efce5736a6ad11e3
Noel <noelcerulean@aol.com>
2024-05-05 15:41:13 -0700

    luminesce test again

--- data/abilities.ts
+++ data/abilities.ts
@@ -2524,7 +2524,7 @@ export const Abilities: {[abilityid: string]: AbilityData} = {
 		num: 203,
 	},
 	luminesce: {
-		onAnyAfterMoveSecondarySelf(pokemon, target, move) {
+		onAnyHit(pokemon, target, move) {
 			if (move.flags['light']) {
 				pokemon.addVolatile('luminesce');
 			}
